f6b568f12d2529ee488651414fa3638a7ba4a394
DanielRamsauer <danramsauer@gmail.com>
2022-07-29 18:33:18 -0400

    added js and styled tech stack page

--- scripts.js
+++ scripts.js
@@ -1,1 +1,33 @@
-console.log('Hello World!');
+const portfolio = {}
+
+console.log(portfolio);
+portfolio.init = function init() {
+    portfolio.displayPortfolio();
+}
+
+portfolio.projectLinks = [
+    {
+        name: 'Currency Converter',
+        livelink: 'https://incandescent-sprinkles-c54daa.netlify.app/',
+        repo: 'https://github.com/Juno-Project-2/Currency-Converter'
+    }
+    {
+        name: 'Captured Landing Page',
+        livelink: 'https://graceful-cajeta-2db571.netlify.app/',
+        repo: 'https://github.com/DanRamsauer/daniel-ramsauer-project-one'
+    }
+]
+
+    
+portfolio.projects = document.querySelector('#projects');
+
+portfolio.displayPortfolio = function () {
+
+    portfolio.projectLinks.forEach(project => {
+        const ul = document.createElement('li')
+    });
+}
+
+
+
+portfolio.init();
