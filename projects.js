8df9ddc4ed95ad280fd7290e1d502d91598281bf
DanielRamsauer <danramsauer@gmail.com>
2022-08-04 09:24:02 -0400

    added notes to myself

--- projects.js
+++ projects.js
@@ -1,46 +1,51 @@
-const projects = {}
-
-projects.init = function init() {
-    projects.displayprojects();
-}
-
-projects.projectLinks = [
+const projectLinks = [
     {
         name: 'Currency Converter',
         livelink: 'https://incandescent-sprinkles-c54daa.netlify.app/',
-        repo: 'https://github.com/Juno-Project-2/Currency-Converter'
+        repo: 'https://github.com/Juno-Project-2/Currency-Converter',
+        img : './assests/Photos/currencyconverterscreenshot.png',
+        description: 'An app that takes a number from the user and has them choose a source currency and target currency and it gives you the conversion rate'
     },
     {
         name: 'Captured Landing Page',
         livelink: 'https://graceful-cajeta-2db571.netlify.app/',
-        repo: 'https://github.com/DanRamsauer/daniel-ramsauer-project-one'
+        repo: 'https://github.com/DanRamsauer/daniel-ramsauer-project-one',
+        img : './assests/Photos/currencyconverterscreenshot.png',
+        description: 'A landing page for a fake website'
     }
 ]
 
     
-projects.projectsUl = document.querySelector('#projects');
+const projectsUl = document.querySelector('#projects');
 
-projects.displayprojects = function () {
+const displayprojects = function () {
 
-    projects.projectLinks.forEach(project => {
+    projectLinks.forEach(project => {
         const li = document.createElement('li');
-        const p = document.createElement('p');
+        const h3 = document.createElement('h3');
         const anchorOne = document.createElement('a');
         const anchorTwo = document.createElement('a');
+        const pTwo = document.createElement('p');
+        const image = document.createElement('img');
 
-        p.innerHTML = project.name;
+        h3.innerText = project.name;
         anchorOne.innerText = 'Live Link';
         anchorTwo.innerText = 'Repo';
         anchorOne.href = project.livelink;
         anchorTwo.href = project.repo;
+        pTwo.innerText = project.description;
+        image.alt = `A screenshot of ${project.name}`;
+        image.src = project.img;
 
-        li.appendChild(p);
+        li.appendChild(h3);
         li.appendChild(anchorOne);
         li.appendChild(anchorTwo);
-        projects.projectsUl.appendChild(li);        
+        li.appendChild(pTwo);
+        li.appendChild(image);
+        projectsUl.appendChild(li);        
     });
 }
 
 
 
-projects.init();
+displayprojects();
