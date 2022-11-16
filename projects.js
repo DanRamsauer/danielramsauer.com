72d7f811bc1592200158600014c18dd23260ce6e
DanielRamsauer <danramsauer@gmail.com>
2022-11-16 13:36:55 -0500

    updated portfolio

--- projects.js
+++ projects.js
@@ -1,6 +1,10 @@
 const projectLinks = [
     {
         name: 'Anime finder',
+        tech1: 'React',
+        tech2: 'HTML',
+        tech3: 'CSS',
+        tech4: 'Firebase',
         livelink: 'https://animes-finder.netlify.app/',
         repo: 'https://github.com/DanRamsauer/daniel-ramsauer-project-three',
         img : './assests/Photos/animeFinder.png',
@@ -9,6 +13,10 @@ const projectLinks = [
     },
     {
         name: 'Book Club Reads',
+        tech1: 'React',
+        tech2: 'HTML',
+        tech3: 'SCSS',
+        tech4: 'Firebase',
         livelink: 'https://junobookclub.netlify.app/',
         repo: 'https://github.com/juno-book-club/book-club',
         img : './assests/Photos/bookClubReads.png',
@@ -17,6 +25,10 @@ const projectLinks = [
     },
     {
         name: 'Currency Converter',
+        tech1: 'React',
+        tech2: 'HTML',
+        tech3: 'SCSS',
+        tech4: 'Firebase',
         livelink: 'https://incandescent-sprinkles-c54daa.netlify.app/',
         repo: 'https://github.com/Juno-Project-2/Currency-Converter',
         img : './assests/Photos/currencyConverter.png',
@@ -25,6 +37,8 @@ const projectLinks = [
     },
     {
         name: 'Captured Landing Page',
+        tech1: 'HTML',
+        tech2: 'CSS',
         livelink: 'https://graceful-cajeta-2db571.netlify.app/',
         repo: 'https://github.com/DanRamsauer/daniel-ramsauer-project-one',
         img : './assests/Photos/Captured.png',
@@ -46,28 +60,48 @@ const displayprojects = function () {
         const anchorOne = document.createElement('a');
         const anchorTwo = document.createElement('a');
         const pTwo = document.createElement('p');
+        const tStack1 = document.createElement('p');
+        const tStack2 = document.createElement('p');
+        const tStack3 = document.createElement('p');
+        const tStack4 = document.createElement('p');
+        const tStackDiv = document.createElement('div');
+        const anchorDiv = document.createElement('div');
 
         h3.innerText = project.name;
-        anchorOne.innerText = 'Live Link';
+        anchorOne.innerText = 'Live';
         anchorTwo.innerText = 'Repo';
         anchorOne.href = project.livelink;
         anchorTwo.href = project.repo;
         pTwo.innerText = project.description;
+        tStack1.innerText = project.tech1;
+        tStack2.innerText = project.tech2;
+        tStack3.innerText = project.tech3;
+        tStack4.innerText = project.tech4;
         img.src = project.img;
         img.alt = `A screenshot of ${project.name}`
 
         li.appendChild(h3);
-        li.appendChild(anchorOne);
-        li.appendChild(anchorTwo);
+        tStackDiv.appendChild(tStack1);
+        tStackDiv.appendChild(tStack2);
+        tStackDiv.appendChild(tStack3);
+        tStackDiv.appendChild(tStack4);
+        anchorDiv.appendChild(anchorOne);
+        anchorDiv.appendChild(anchorTwo);
+        li.appendChild(tStackDiv);
         li.appendChild(pTwo);
         div.appendChild(img);
         div.appendChild(li);
+        li.appendChild(anchorDiv);
         projectsUl.appendChild(div);
         
         div.classList.add(project.class);
+        tStackDiv.classList.add('techStack');
+        tStack1.classList.add('flicker');
+        tStack2.classList.add('flicker');
+        tStack3.classList.add('flicker');
+        tStack4.classList.add('flicker');
+        anchorDiv.classList.add('links');
         li.classList.add('descriptions');
-        // anchorOne.classList.add('flicker');
-        // anchorTwo.classList.add('flicker');
         anchorOne.setAttribute('target', '_blank');
         anchorTwo.setAttribute('target', '_blank');
     });
